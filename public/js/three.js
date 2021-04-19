1bb233aae12029574e2eab9af70d8c1b05ff9dc0
severin0038 <severin.peyer@students.fhnw.ch>
2021-04-19 07:51:10 +0200

    add different camera positions

--- public/js/three.js
+++ public/js/three.js
@@ -1,4 +1,3 @@
-
 const renderer = new THREE.WebGLRenderer({
     alpha: true
 });
@@ -7,27 +6,26 @@ document.getElementById("canvasContainer").appendChild( renderer.domElement );
 
 
 const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
+camera.position.set(0,1,5);
+if(Math.random() >= 0.5) { camera.position.z = -5 }
 camera.lookAt( 0, 0, 0 );
 
 const scene = new THREE.Scene();
 
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0xff00ff } );
+const geometry = new THREE.BoxGeometry(1, 0.5, 2);
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material2 = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
-const line = new THREE.Line( geometry, material );
-
-scene.add( line );  
+const cube = new THREE.Mesh( geometry, material );
+cube.position.set(0,0,1);
+scene.add( cube );
+const cube2 = new THREE.Mesh( geometry, material2 );
+cube2.position.set(0,0,-1);
+scene.add( cube2 );
 
 function animate() {
     requestAnimationFrame( animate );
+    cube.rotation.y += 0.01;
     renderer.render( scene, camera );
 }
 
