13f1ff1c7bd6f7dafc37684acd2da04724bbfa32
Daniel Obrist <daniel.obrist@students.fhnw.ch>
2021-04-25 18:22:39 +0200

    Drag and TransformControls for cube to test position updates

--- public/js/three.js
+++ public/js/three.js
@@ -1,7 +1,13 @@
 import  * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 import {GLTFLoader} from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
+import {DragControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/DragControls.js';
+import { TransformControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/TransformControls.js';
+
 // import * as THREE from './../../node_modules/three/build/three.module.js';
 export {changeCameraPosition};
+
+let draggableObjects = [];
+let control;
 
 const renderer = new THREE.WebGLRenderer({
     alpha: true
@@ -26,7 +32,7 @@ scene.add( cube );
 const cube2 = new THREE.Mesh( geometry, material2 );
 cube2.position.set(0,0,-1);
 scene.add( cube2 );
-
+draggableObjects.push(cube2);
 
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 scene.add( directionalLight );
@@ -43,6 +49,19 @@ loader.load( '../assets/banana.glb', function ( gltf ) {
 	console.error( error );
 });
 
+scene.add( new THREE.GridHelper( 1000, 10, 0x888888, 0x444444 ) );
+
+
+const mouse = new THREE.Vector2(), raycaster = new THREE.Raycaster();
+// const controls = new DragControls( [ ...draggableObjects ], camera, renderer.domElement );
+// controls.addEventListener( 'drag', render );
+control = new TransformControls( camera, renderer.domElement );
+control.showY = false;
+control.addEventListener( 'change', render );
+
+control.attach(cube2);
+scene.add(control);
+
 function animate() {
     requestAnimationFrame( animate );
     cube.rotation.y += 0.01;
@@ -53,6 +72,12 @@ function changeCameraPosition() {
     console.log('changing camera position!');
     camera.position.z = -5;
     camera.lookAt( 0, 0, 0 );
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
 }
 
 animate();
